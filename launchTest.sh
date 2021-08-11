52d86f8ea2cd814be80a65b40107e03af824f38a
Benoit CIVEL <bcivel@users.noreply.github.com>
2021-08-12 00:32:38 +0200

    Update launchTest.sh

--- launchTest.sh
+++ launchTest.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-curl --request POST --url "https://prod.cerberus-testing.com/AddToExecutionQueueV003" -d campaign=TestBCI -d tag=tutu
+###Get CampaignName and Cerberus Host
+while getopts c:h:n: flag
+do
+    case "${flag}" in
+        c) committer=${OPTARG};;
+        h) host=${OPTARG};;
+        n) campaignName=${OPTARG};;
+    esac
+done
+echo "Committer: $committer";
+echo "Cerberus Host: $host";
+echo "Campaign Name: $campaignName";
 
+###Generate Tag using Campaign Name, Commiter and UnixTimestamp
+tag=$campaignName.$(date +%s)
 
+###Run Campaign
+curl --request POST --url "$host/AddToExecutionQueueV003" -d campaign=$campaignName -d tag=$tag
+
+###Loop on resultCI Until end of campaign
 num=0
 while [ $num -lt 3 ]
 do
-    result=$(curl --request POST --url "https://prod.cerberus-testing.com/ResultCIV004" -d tag=tutu | jq -r '.result')
+    result=$(curl --request POST --url "$host/ResultCIV004" -d tag=$tag | jq -r '.result')
     echo $result
     if [[ "$result" != "PE" ]]; then
         break
