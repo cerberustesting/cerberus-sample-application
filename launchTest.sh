90c1b19da268b3c3e30cd834bfd17d8f5a88bcae
Benoit CIVEL <bcivel@users.noreply.github.com>
2021-08-11 17:46:36 +0200

    Update launchTest.sh

--- launchTest.sh
+++ launchTest.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 curl --request POST --url "https://prod.cerberus-testing.com/AddToExecutionQueuePrivate" -d campaign=TestBCI -d tag=tutu --cookie "JSESSIONID=B406EC02B7FE44F76240BEDE70C6B93C"
 
@@ -8,14 +8,14 @@ while [ $num -lt 3 ]
 do
     result=$(curl --request POST --url "https://prod.cerberus-testing.com/ResultCIV004" -d tag=tutu --cookie "JSESSIONID=B406EC02B7FE44F76240BEDE70C6B93C" | jq -r '.result')
     echo $result
-    if [[ "$result" -ne "PE" ]]; then
+    if [[ "$result" != "PE" ]]; then
         break
     fi
     sleep 3
     echo "One more try"
 done
 
-if [[ "$result" -ne "OK" ]]; then
+if [[ "$result" != "OK" ]]; then
     exit 1
 fi
 echo $result
